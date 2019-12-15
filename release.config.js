fc5f87e5671b9ad58d013d7176d4c1c863cc6d9f
andreafalzetti <afalzetti@gmail.com>
2019-12-15 18:02:37 +0000

    ci: add semantic release

---
+++ release.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  branch: 'master',
+  // eslint-disable-next-line no-template-curly-in-string
+  tagFormat: '${version}',
+  dryRun: false,
+  ci: true,
+  debug: true,
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope']
+        }
+      }
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ]
+};
